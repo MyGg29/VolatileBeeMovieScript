df641a4f36df6b7e24138ab827925b1529cbe2d9
MyGg29 <gwendalgourmelon@gmail.com>
2019-12-13 18:58:09 +0100

    managed rate limit remaining

--- beemoviescript.py
+++ beemoviescript.py
@@ -22,7 +22,7 @@ beeMovieBlocks = split_string(text=beemoviescript, blocksize=255)
 for idx, scriptBlock in enumerate(beeMovieBlocks):
     r = requests.get(f"https://volatile.wtf/?key=BeeMovieScript{idx}&val={scriptBlock}")
     print(f"Done {idx}")
-    if(r.headers['X-Rate-Limit-Reset']):
+    if(r.headers['X-Rate-Limit-Remaining'] == "0"):
         print("Write limit reached")
         print(f"Sleeping for {r.headers['X-Rate-Limit-Reset']} seconds")
         time.sleep(int(r.headers['X-Rate-Limit-Reset']))
